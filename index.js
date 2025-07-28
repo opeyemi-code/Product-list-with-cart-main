2d53cf897c522f0b947ce657e4fc25fc4b92bf96
Obatola Opeyemi <opeyemiobatola@gmail.com>
2025-07-28 20:00:24 +0100

    Chore: Initial file scalfolding

---
+++ index.js
@@ -0,0 +1,7 @@
+// import data from "./data.json" assert { type: "json" };
+
+// fetch("./data.json")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+// console.log(data);
